a98e137c492fc023294636b24ae4003f42a30030
claraifpereira <claraifpereira@gmail.com>
2018-02-15 10:01:10 +0000

    My Django Girls app, first commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\clarap~1\onedrive\12-slp~1\12-12_~1\1245cc~1\django~1\project\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
